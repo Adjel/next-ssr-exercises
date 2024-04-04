68804385517cf7ddf58dcc89a4dff38d8a0389c6
Adjel <adjel.touaoula@gmail.com>
2024-04-04 14:35:03 +0200

    exercise

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -1,11 +1,13 @@
-'use client';
-import React from 'react';
-import format from 'date-fns/format';
+"use client";
+import React from "react";
+import format from "date-fns/format";
+import Spinner from "../../../components/Spinner/Spinner";
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+  const [time, setTime] = React.useState(null);
 
   React.useEffect(() => {
+    setTime(new Date());
     const intervalId = window.setInterval(() => {
       setTime(new Date());
     }, 50);
@@ -16,7 +18,9 @@ function Clock() {
   }, []);
 
   return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
+    <p className="clock">
+      {time ? format(time, "hh:mm:ss.S a") : Spinner("black")}
+    </p>
   );
 }
 
