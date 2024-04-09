abbb41ace347787e3d0554e04590dd093d9bf70d
Adjel <adjel.touaoula@gmail.com>
2024-04-09 13:29:49 +0200

    tried but not succeded

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,32 +1,31 @@
-'use client';
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+"use client";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
-import Interview from './Interview';
-import './styles.css';
+import Interview from "./Interview";
+import "./styles.css";
 
 function InterviewExercise() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
+  let isDesktop = useMediaQuery({
+    query: "(min-width: 500px)",
   });
+
+  React.useEffect(() => {
+    isDesktop = false;
+  }, []);
 
   return (
     <main>
       <Interview />
       {isDesktop && (
         <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
+          <img src="/gwen-artist.png" alt="Portrait of the artist" />
           <h2>About the Artist</h2>
           <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
+            Gwen Altaria is a contemporary artist known for her unique blend of
+            traditional oil painting techniques and pop culture references. Born
+            and raised in London, England, Vivi had an early love for both the
+            fine arts and the fantastical worlds of video games.
           </p>
         </aside>
       )}
