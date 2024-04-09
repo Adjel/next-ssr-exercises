3788a47b5093ae7f03ea0a6801ccafe917546624
Adjel <adjel.touaoula@gmail.com>
2024-04-09 13:40:16 +0200

    tried correction after had watch the solution

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,36 +1,32 @@
 "use client";
 import React from "react";
 import { useMediaQuery } from "react-responsive";
-
+import styled from "styled-components";
 import Interview from "./Interview";
 import "./styles.css";
 
 function InterviewExercise() {
-  let isDesktop = useMediaQuery({
-    query: "(min-width: 500px)",
-  });
-
-  React.useEffect(() => {
-    isDesktop = false;
-  }, []);
-
   return (
     <main>
       <Interview />
-      {isDesktop && (
-        <aside>
-          <img src="/gwen-artist.png" alt="Portrait of the artist" />
-          <h2>About the Artist</h2>
-          <p>
-            Gwen Altaria is a contemporary artist known for her unique blend of
-            traditional oil painting techniques and pop culture references. Born
-            and raised in London, England, Vivi had an early love for both the
-            fine arts and the fantastical worlds of video games.
-          </p>
-        </aside>
-      )}
+      <ResponsiveAside>
+        <img src="/gwen-artist.png" alt="Portrait of the artist" />
+        <h2>About the Artist</h2>
+        <p>
+          Gwen Altaria is a contemporary artist known for her unique blend of
+          traditional oil painting techniques and pop culture references. Born
+          and raised in London, England, Vivi had an early love for both the
+          fine arts and the fantastical worlds of video games.
+        </p>
+      </ResponsiveAside>
     </main>
   );
 }
+
+const ResponsiveAside = styled.aside`
+  @media (max-width: 500px) {
+    display: none;
+  }
+`;
 
 export default InterviewExercise;
